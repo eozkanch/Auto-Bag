4783593576eaf3759f8821b8b73d56b13ab9a9b6
Hldka <116027001+Hldka@users.noreply.github.com>
2023-06-26 18:06:40 +0200

    v2

--- my-bag/src/app/page.js
+++ my-bag/src/app/page.js
@@ -1,12 +1,12 @@
 "use client";
 import { Button } from 'react-bootstrap'
+import Navbar from '../../components/01-navbar/navbar';
 
 export default function Home() {
   return (
    <>
-   <h1>Biz Basliyoruz </h1>
    
-   <Button> merhaba</Button>
+  
    
    </>
   )

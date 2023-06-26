a227d173bb68f435d885e2b0207d50e27114eded
Hldka <116027001+Hldka@users.noreply.github.com>
2023-06-26 17:59:31 +0200

    v1

---
+++ my-bag/src/app/page.js
@@ -0,0 +1,13 @@
+"use client";
+import { Button } from 'react-bootstrap'
+
+export default function Home() {
+  return (
+   <>
+   <h1>Biz Basliyoruz </h1>
+   
+   <Button> merhaba</Button>
+   
+   </>
+  )
+}

edae74a2ed661c91a1a6ad2753d6659c907c37a0
Hldka <116027001+Hldka@users.noreply.github.com>
2023-06-27 17:09:34 +0200

    v2

---
+++ my-bag/components/04-productList/shop.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Footer from './footer'
+import ProductList from './Product-list'
+
+const Shop = () => {
+  return (
+    <div>
+<Header/>
+<ProductList/>
+
+
+    </div>
+  )
+}
+
+export default Shop
